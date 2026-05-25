c6f0820cc73bf48f333caf1e0dc28b139d06af56
prasad96922 <prasadthaduka@gmail.com>
2026-05-25 15:56:49 +0530

    module2.1.2-springBootBackendWebApplication

---
+++ src/main/java/com/lvprasad/springbootwebapplication/springbootwebapp/SpringbootwebappApplication.java
@@ -0,0 +1,13 @@
+package com.lvprasad.springbootwebapplication.springbootwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootwebappApplication.class, args);
+	}
+
+}
